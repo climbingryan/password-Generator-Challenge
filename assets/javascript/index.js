08fa03edf4139ede532080ff21de786f554074b6
shaggy <simmons5family@gmail.com>
2020-10-11 00:41:49 -0500

    if you say no to prompt. it returns nothing to password

--- assets/javascript/index.js
+++ assets/javascript/index.js
@@ -1,7 +1,5 @@
 
 function generatePassword() {
-
-   
         //accepts user input for length
     var lengthPrompt = function() {
         var length = prompt("How many characters do you want? You can choose any number between 8 & 128");
@@ -18,24 +16,40 @@ function generatePassword() {
     var length = lengthPrompt(),
     charSetLower = "abcdefghijklmnopqrstuvwxyw",
     charSetUpper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+    numbers = 123456789,
     retVal = "";
     retValUp = "";
 
-
-    var upCase = window.prompt("Would You like uppercase letters? Yes or No");
-    if (upCase === "YES" || upCase === "yes" || upCase === "Yes") {
-        for (var i = 0, n = charSetUpper.length; i < length; ++i) {
-            retValUp += charSetUpper.charAt(Math.floor(Math.random() * n))
+        // UpperCase Function
+    var upperCaseF = function() {
+            // let's us have a password entirly uppercase
+        var upCase = window.prompt("Would You like uppercase letters? Yes or No");
+        if (upCase === "YES" || upCase === "yes" || upCase === "Yes") {
+            for (var i = 0, n = charSetUpper.length; i < length; ++i) {
+                retValUp += charSetUpper.charAt(Math.floor(Math.random() * n))
+            }
+            return retValUp;
+        } else if (upCase === "NO" || upCase === "no" || upCase === "No" ){
+            retValUp === "";
+            return retValUp;
+        }  
+    };
+        // LowerCase Function
+    var lowerCaseF = function() {
+        // loop for lower case letters
+        var lowCase = window.prompt("Would you like lowercase letter? Yes or No?");
+        if (lowCase === "YES" || lowCase === "yes" || lowCase === "Yes") {
+            for (var i = 0, n = charSetLower.length; i < length; ++i) {
+                retVal += charSetLower.charAt(Math.floor(Math.random() * n));
+            }
+        return retVal;
+        } else if (lowCase === "NO" || lowCase === "no" || lowCase === "No") {
+            retVal === "";
+            return retVal;
         }
-        console.log(retValUp);
-        return retValUp;
     }
-    
-
-    for (var i = 0, n = charSetLower.length; i < length; ++i) {
-        retVal += charSetLower.charAt(Math.floor(Math.random() * n));
-    }
-    return retVal;
+        // combines all functions
+    return lowerCaseF() + upperCaseF();
 }
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
@@ -68,3 +82,9 @@ generateBtn.addEventListener("click", writePassword);
         }
         console.log(charPrompt());
     } */
+
+    /* 
+    if (retValUp === "A" || retValUp === "B" || retValUp === "C" || retValUp === "D" || retValUp === "E" || retValUp === "F" || retValUp === "G" || retValUp === "H" || retValUp === "I" || retValUp === "J" || retValUp === "K" 
+    || retValUp === "L" || retValUp === "M" || retValUp === "N" || retValUp === "O" || retValUp === "P" || retValUp === "Q" || retValUp === "R" || retValUp === "S" || retValUp === "T" || retValUp === "U" || retValUp === "V" || 
+    retValUp === "W" || retValUp === "X" || retValUp === "Y" || retValUp === "Z")
+    */
