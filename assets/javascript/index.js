268fc15471524e6c85381c2b6c395382a55fc924
shaggy <simmons5family@gmail.com>
2020-10-11 16:30:28 -0500

    re-organize variables. Put most global into resepctive functions

--- assets/javascript/index.js
+++ assets/javascript/index.js
@@ -3,36 +3,34 @@ function generatePassword() {
         //accepts user input for length
     var lengthPrompt = function() {
         var length = prompt("How many characters do you want? You can choose any number between 8 & 128");
-            // chooses what to pass and accept
+            // if nothing is typed, try again
         if (length === "" || length === null || length <= 7 || length >= 129) {
             alert("Invalid answer. Try Again")
             return lengthPrompt();
         } else {
+            // return user number input
             console.log(length);
             return length;
         }
     }
 
-    var length = lengthPrompt(),
-    charSetLower = "abcdefghijklmnopqrstuvwxyw",
-    charSetUpper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
-    numberSet = "123456789",
-    specialSet = "!#$%&'()*+,-./:;/<=>?@[]^_`{|}~",
-    retSpecial = "",
-    retNum = null,
-    retVal = "",
-    retValUp = "",
-    retValCom = "";
+        // global variable for functions
+    var length = lengthPrompt();
 
         // UpperCase Function
     var upperCaseF = function() {
-            // let's us have a password entirly uppercase
+        var charSetUpper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        var retValUp = "";
+
         var upCase = window.prompt("Would You like uppercase letters? Yes or No");
+
+            // if yes, return randomized string
         if (upCase === "YES" || upCase === "yes" || upCase === "Yes") {
             for (var i = 0, n = charSetUpper.length; i < length; ++i) {
                 retValUp += charSetUpper.charAt(Math.floor(Math.random() * n))
             }
             return retValUp;
+                // if no, Return nothing
         } else if (upCase === "NO" || upCase === "no" || upCase === "No" ){
             retValUp === null;
             return retValUp;
@@ -41,13 +39,18 @@ function generatePassword() {
 
         // LowerCase Function
     var lowerCaseF = function() {
-        // loop for lower case letters
+        var charSetLower = "abcdefghijklmnopqrstuvwxyw";
+        var retVal = "";
+
         var lowCase = window.prompt("Would you like lowercase letters? Yes or No?");
+        
+            // if yes, return randomized string
         if (lowCase === "YES" || lowCase === "yes" || lowCase === "Yes") {
             for (var i = 0, n = charSetLower.length; i < length; ++i) {
                 retVal += charSetLower.charAt(Math.floor(Math.random() * n));
             }
         return retVal;
+            // if no, Return nothing
         } else if (lowCase === "NO" || lowCase === "no" || lowCase === "No") {
             retVal === null;
             return retVal;
@@ -56,12 +59,18 @@ function generatePassword() {
 
         // Numbers Function
     var numbersF = function() {
+        var numberSet = "123456789";
+        var retNum = null;
+        
         var numPrompt = window.prompt("Would you like numbers? Yes or No?");
+        
+        // if yes, return randomized string
         if (numPrompt === "YES" || numPrompt === "yes" || numPrompt === "yes") {
             for (var i = 0, n = numberSet.length; i < length; ++i) {
                 retNum += numberSet.charAt(Math.floor(Math.random() * n));
             }
             return retNum;
+                // if no, Return nothing
         } else if (numPrompt === "NO" || numPrompt === "no" || numPrompt === "No") {
             retNum === null;
             return retNum;
@@ -70,18 +79,26 @@ function generatePassword() {
 
         // Special Characters Funcion
     var specialChar = function () {
+        var specialSet = "!#$%&'()*+,-./:;/<=>?@[]^_`{|}~";
+        var retSpecial = "";
+
         var specialPrompt = window.prompt("Would you like special characters? Yes or No?");
+        
+        // if yes, return randomized string
         if (specialPrompt === "YES" || specialPrompt === "yes" || specialPrompt === "Yes") {
             for (var i = 0, n = specialSet.length; i < length; ++i) {
                 retSpecial += specialSet.charAt(Math.floor(Math.random() * n));
             }
             return retSpecial;
+                // if no, Return nothing
         } else if (specialPrompt === "NO" || specialPrompt === "no" || specialPrompt === "No") {
             retSpecial === null;
             return retSpecial;
         }
     };
 
+
+    var retValCom = "";
         // combines all functions
     var alphabet = lowerCaseF() + upperCaseF() + numbersF() + specialChar();    
         // randomly mixes alphabet
